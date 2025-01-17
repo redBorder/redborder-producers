7817beb945b4a9dc7218f02f0022640e78e8f1fb
Luis Blanco <ljblanco@redborder.com>
2025-01-17 12:15:04 +0000

    optimize

--- resources/producers/py/producer.py
+++ resources/producers/py/producer.py
@@ -2,15 +2,10 @@ from kafka import KafkaProducer
 import json
 import time
 import random
-# from traffic import generate_event
-
-# # Define sig_ids list
-# sig_ids = [1, 2, 3, 4, 5]  # Add appropriate signature IDs
 
 # Configura el productor de Kafka
 producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
                         value_serializer=lambda v: json.dumps(v).encode('utf-8'))
-
 
 """
 Run a Kafka producer that sends data to a specified topic.
