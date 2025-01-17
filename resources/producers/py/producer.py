4b8067355570675e1daf904078db546bb4cdffcf
Luis Blanco <ljblanco@redborder.com>
2025-01-17 12:13:00 +0000

    add description

--- resources/producers/py/producer.py
+++ resources/producers/py/producer.py
@@ -12,6 +12,15 @@ producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
                         value_serializer=lambda v: json.dumps(v).encode('utf-8'))
 
 
+"""
+Run a Kafka producer that sends data to a specified topic.
+
+Args:
+    callback: Function that generates the data to be sent
+    duration: Time in seconds to run the producer. If negative, runs indefinitely
+    topic: Kafka topic to send data to (default: 'rb_flow')
+    time_range: Tuple of (min, max) seconds between events (default: (0.000001, 0.01))
+"""  
 def run_producer(callback, duration, topic='rb_flow', time_range=(0.000001, 0.01)):
   start_time = time.time()
   try:
