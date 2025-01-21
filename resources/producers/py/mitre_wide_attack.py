9e96137f51ad67e04239810bd13e0f9838b97eee
Luis Blanco <ljblanco@redborder.com>
2025-01-21 10:55:35 +0000

    fix coordinator

--- resources/producers/py/mitre_wide_attack.py
+++ resources/producers/py/mitre_wide_attack.py
@@ -20,18 +20,16 @@ SCRIPTS_PATH = [
   '/usr/lib/redborder/producers/py/at1_reconocimiento_scan.py',
   '/usr/lib/redborder/producers/py/at2_resource_development_ssl.py',
   '/usr/lib/redborder/producers/py/at3.py',
-  '/etc/synthetic_producer/config/vault_new_powershell.yml',
   '/usr/lib/redborder/producers/py/at5_persistence_backdoor.py',
   '/usr/lib/redborder/producers/py/at6.py',
   '/usr/lib/redborder/producers/py/at7_defense_evasion.py',
   '/usr/lib/redborder/producers/py/at8_credential_access.py',
-  '/usr/lib/redborder/producers/py/at9_lateral_movement.py',
   '/usr/lib/redborder/producers/py/at9_discovery.py',
-  '/usr/lib/redborder/producers/py/at10_collection_screen.py',
-  '/usr/lib/redborder/producers/py/at11_exfiltration_dropbox.py',
+  '/usr/lib/redborder/producers/py/at10_lateral_movement.py',
+  '/usr/lib/redborder/producers/py/at11_collection_screen.py',
   '/usr/lib/redborder/producers/py/at12_cnc.py',
-  '/usr/lib/redborder/producers/py/at13_impact_ransom.py'
-]
+  '/usr/lib/redborder/producers/py/at13_exfiltration_dropbox.py',
+  '/usr/lib/redborder/producers/py/at14_impact_ransom.py']
 if __name__ == "__main__":
   parser = argparse.ArgumentParser(description=__doc__)
   parser.add_argument('-f', '--fast', action='store_true', default=False, help='Run with no wait between incidents')
