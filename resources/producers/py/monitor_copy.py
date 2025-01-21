60b49dfbb0123fddf430ea5995e5780f7fc0afbd
Luis Blanco <ljblanco@redborder.com>
2025-01-21 15:25:26 +0000

    remove typo

--- resources/producers/py/monitor_copy.py
+++ resources/producers/py/monitor_copy.py
@@ -99,4 +99,3 @@ if __name__ == "__main__":
     parser.add_argument('-d', '--duration', type=int, default=5, help='Duration in seconds (default: 5), -1 for infinite')
     args = parser.parse_args()
     run_producer(args.duration)
-~                                          
