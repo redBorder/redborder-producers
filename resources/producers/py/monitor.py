e5578fac8bee2839fd50eec06a8fa45eea8e4364
Luis Blanco <ljblanco@redborder.com>
2025-01-21 15:34:50 +0000

    chosea aeach type of monitor one by one (by timestamp)

--- resources/producers/py/monitor.py
+++ resources/producers/py/monitor.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 import argparse
 import time
-import random
-import assets
 from sensors import get_random_sensor
 from producer import run_producer
 from monitor_event import MonitorEvent
 
-def generate_event(monitor):
-  sensor = random.choice(assets.mirror_devices)
+def generate_event():
+  #TODO: All monitors, not just one by one
+  monitors = MonitorEvent.generate_all_monitors()
+  monitor = monitors[int(time.time()) % monitors.__len__()]
   return {
     'index_partitions':5,
     'index_replicas':1,
@@ -21,9 +21,8 @@ def generate_event(monitor):
   }
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-d', '--duration', type=int, default=-1, help='Duration in seconds (default: 5), -1 for infinite')
-    parser.add_argument('-l', '--looptime', type=int, default=60, help='Loop time in seconds (default: 300)') # 5 minutes
-    args = parser.parse_args()
-    for monitor in MonitorEvent.generate_all_monitors():
-      run_producer(callback=generate_event(monitor), duration=args.duration, topic='rb_monitor', time_range=(args.looptime, args.looptime)) 
+  parser = argparse.ArgumentParser()
+  parser.add_argument('-d', '--duration', type=int, default=-1, help='Duration in seconds (default: 5), -1 for infinite')
+  parser.add_argument('-l', '--looptime', type=int, default=1, help='Loop time in seconds (default: 300)') # 1 sec until fix monitor multi event
+  args = parser.parse_args()
+  run_producer(callback=generate_event, duration=args.duration, topic='rb_monitor', time_range=(args.looptime, args.looptime))
