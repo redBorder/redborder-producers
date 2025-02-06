0600d20f383d9257000ce119ebdf9b61e7f2ee7e
Luis Blanco <ljblanco@redborder.com>
2025-02-06 12:42:00 +0000

    set executable mode, lint

--- cap_manager.rb
+++ cap_manager.rb
@@ -1,13 +1,16 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
+
 require 'optparse'
 
 def usage
   puts "Usage: #{$0} [options]"
-  puts "Options: -p: directory of pcaps"
-  puts "         -c: path to pmacct config file"
-  puts "running this script will manage differences between caps or pcaps and make safe rewrites over the original ips"
-  puts "then it will start replaying the pcaps, hoping somebody is listening, like a flow sensor."
+  puts 'Options: -p: directory of pcaps'
+  puts '         -c: path to pmacct config file'
+  puts 'running this script will manage differences between caps or pcaps and make safe rewrites over the original ips'
+  puts 'then it will start replaying the pcaps, hoping somebody is listening, like a flow sensor.'
 end
+
 def parse_args
   options = {}
   parser = OptionParser.new do |opts|
@@ -52,28 +55,28 @@ end
 def put_interface_up()
   interface_exists = system("ip link show eth1 > /dev/null 2>&1")
   unless interface_exists
-    puts "Creating dummy interface eth1..."
-    system("sudo modprobe dummy")
-    system("sudo ip link add eth1 type dummy")
-    system("sudo ifconfig eth1 hw ether 00:11:22:33:44:55")
+    puts 'Creating dummy interface eth1...'
+    system('sudo modprobe dummy')
+    system('sudo ip link add eth1 type dummy')
+    system('sudo ifconfig eth1 hw ether 00:11:22:33:44:55')
   end
 
   is_ip_configured = system("ip addr show eth1 | grep '10.1.32.201' > /dev/null 2>&1")
   unless is_ip_configured
-    puts "Configuring IP address for eth1..."
-    system("sudo ip addr add 10.1.32.201/24 dev eth1 label eth1:0")
+    puts 'Configuring IP address for eth1...'
+    system('sudo ip addr add 10.1.32.201/24 dev eth1 label eth1:0')
   end
 
   is_interface_up = system("ip link show eth1 | grep 'UP' > /dev/null 2>&1")
   unless is_interface_up
-    puts "Bringing up interface eth1..."
-    system("sudo ip link set eth1 up")
+    puts 'Bringing up interface eth1...'
+    system('sudo ip link set eth1 up')
   end
 
   is_promisc = system("ip link show eth1 | grep 'PROMISC' > /dev/null 2>&1")
   unless is_promisc
-    puts "Setting eth1 to promiscuous mode..."
-    system("sudo ip link set eth1 promisc on")
+    puts 'Setting eth1 to promiscuous mode...'
+    system('sudo ip link set eth1 promisc on') 
   end
 end
 
